0934800177b76acb80853b59d7a1ecb2afffb6ad
Fernando Rego <fernandoluisrego@gmail.com>
2022-03-15 21:20:33 +0000

    Timer lab done

--- lab2/timer.c
+++ lab2/timer.c
@@ -5,43 +5,113 @@
 
 #include "i8254.h"
 
+int timer_hook_id = 0;
+
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  uint8_t st;
+
+  if (timer_get_conf(timer, &st) != OK) {
+    return !OK;
+  }
+
+  uint8_t cmd = 0x0;
+
+  cmd = (st & 0xf) | TIMER_LSB_MSB | BIT(4) | (timer << 6);
+
+  sys_outb(TIMER_CTRL, cmd);
+
+  uint16_t set_freq = TIMER_FREQ / freq;
+
+  uint8_t msb, lsb;
+  util_get_MSB(set_freq,&msb);
+  util_get_LSB(set_freq,&lsb);
+
+  sys_outb(TIMER_0 + timer,lsb);
+  sys_outb(TIMER_0 + timer,msb);
 
   return 1;
 }
 
 int (timer_subscribe_int)(uint8_t *bit_no) {
-    /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
 
-  return 1;
+  timer_hook_id = *bit_no = TIMER0_IRQ;
+  *bit_no = BIT(timer_hook_id);
+
+  sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &timer_hook_id);
+
+  return OK;
 }
 
 int (timer_unsubscribe_int)() {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
 
-  return 1;
+  sys_irqrmpolicy(&timer_hook_id);
+
+  return OK;
 }
 
 void (timer_int_handler)() {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+
 }
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  uint8_t cmd = 0x0;
 
-  return 1;
+  cmd = cmd | TIMER_RB_CMD | TIMER_RB_COUNT_ | TIMER_RB_SEL(timer);
+
+  if (sys_outb(TIMER_CTRL ,cmd) != OK) {
+    return !OK;
+  }
+
+  return util_sys_inb(TIMER_0 + timer,st);
 }
 
-int (timer_display_conf)(uint8_t timer, uint8_t st,
-                        enum timer_status_field field) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+int (timer_display_conf)(uint8_t timer, uint8_t st, enum timer_status_field field) {
 
-  return 1;
+  union timer_status_field_val u;
+
+  uint8_t buf;
+
+  switch(field) {
+    case tsf_all:
+      u.byte = st;
+      break;
+    case tsf_initial:
+      buf = (st & (BIT(4) | BIT(5))) >> 4;
+
+      if (buf == 0x1) {
+        u.in_mode = LSB_only;
+      } else if (buf == 0x2) {
+        u.in_mode = MSB_only;
+      } else if (buf == 0x3) {
+        u.in_mode = MSB_after_LSB;
+      } else {
+        u.in_mode = INVAL_val;
+      }
+
+      break;
+    case tsf_mode:
+      buf =  (st & (BIT(1) | BIT(2) | BIT(3))) >> 1;
+
+      if (buf == 0x0) {
+        u.count_mode = 0;
+      } else if (buf == 0x1) {
+        u.count_mode = 1;
+      } else if (buf == 0x2 || buf == 0x6) {
+        u.count_mode = 2;
+      } else if (buf == 0x3 || buf == 0x7) {
+        u.count_mode = 3;
+      } else if (buf == 0x4) {
+        u.count_mode = 4;
+      } else if (buf == 0x5) {
+        u.count_mode = 5;
+      }
+
+      break;
+    case tsf_base:
+      u.bcd = (st & BIT(0));
+      break;
+  }
+
+  
+  return timer_print_config(timer,field,u);
 }
--- lab2/utils.c
+++ lab2/utils.c
@@ -3,22 +3,24 @@
 #include <stdint.h>
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  if (lsb == NULL) return !OK;
 
-  return 1;
+  *lsb = (uint8_t)(val);
+  return OK;
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  if (msb == NULL) return !OK;
 
-  return 1;
+  *msb = (uint8_t)(val >> 8);
+  return OK;
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  uint32_t byte;
+  int ret = sys_inb(port, &byte);
 
-  return 1;
+  *value = (uint8_t)byte; 
+
+  return ret;
 }
