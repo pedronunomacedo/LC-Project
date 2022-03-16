dcb9bfc1edc18e45884db74eb824938afbe1e98b
Fernando Rego <fernandoluisrego@gmail.com>
2022-03-16 20:06:31 +0000

    Start keyboard kbd_test_scan

--- lab3/keyboard.h
+++ lab3/keyboard.h
@@ -1,5 +1,11 @@
 #pragma once
 
 #include <lcom/lcf.h>
+#include <lcom/lab3.h>
+#include <i8042.h>
 
 void (kbc_ih)(void);
+
+int (kbd_subscribe_int)(uint8_t *bit_no);
+
+int (kbd_unsubscribe_int)();
