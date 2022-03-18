b0e3784429a4acb5d369f104422ab26d51e08c41
Fernando Rego <fernandoluisrego@gmail.com>
2022-03-18 14:01:18 +0000

    Finish kbd_test_poll for lab3

--- lab3/i8042.h
+++ lab3/i8042.h
@@ -2,6 +2,8 @@
 
 #include <lcom/lcf.h>
 
+#define DELAY 20000
+
 #define KBD_ESQ_BC 0x81 //ESQ breakcode
 #define KBD_TWOBYTE_CODE 0xE0 //first byte of a two-byte scancode
 #define KBD_MSB BIT(7) //MSB of a byte
@@ -10,6 +12,7 @@
 
 #define KBC_ST_REG 0x64 //keyboard status register
 #define KBC_OBF BIT(0) //status register output buffer full bit
+#define KBC_AUX BIT(5) //status register AUX bit
 #define KBC_PARITY_ERR BIT(7) //status register parity error bit
 #define KBC_TIMEOUT_ERR BIT(6) //status register timeout error bit
 
@@ -19,3 +22,5 @@
 
 #define KBC_CMD_RCB 0x20 //keyboard read command byte
 #define KBC_CMD_WCB 0x60 //keyboard write command byte
+
+#define KBC_INT BIT(0) //command byte enable interrupt on keyboard 
