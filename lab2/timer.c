72e1c2143115763e6256302f6390bc50a5ec0f45
pedronunomacedo <up202007531@edu.fe.up.pt>
2022-04-20 21:48:07 +0100

    Added some files of lab3 to correct an error

--- lab2/timer.c
+++ lab2/timer.c
@@ -13,9 +13,8 @@ int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
 }
 
 int (timer_subscribe_int)(uint8_t *bit_no) {
-    /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
+  
+  
   return 1;
 }
 
