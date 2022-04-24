935df7984641b4a1749f8f57551fcec2d4132bb7
Fernando Rego <fernandoluisrego@gmail.com>
2022-04-24 23:36:38 +0100

    Add second function of lab4

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -97,9 +97,72 @@ int (mouse_test_packet)(uint32_t cnt) {
 }
 
 int (mouse_test_async)(uint8_t idle_time) {
-    /* To be completed */
-    printf("%s(%u): under construction\n", __func__, idle_time);
-    return 1;
+    if (set_mouse_data_reporting(true) != OK) {
+    printf("mouse_test_packet: set_mouse_data_reporting failed\n");
+    return !OK;
+  }
+
+  int r, ipc_status;
+  message msg;
+
+  uint8_t mouse_bit_no, timer_bit_no;
+  if (mouse_subscribe_int(&mouse_bit_no) != OK || timer_subscribe_int(&timer_bit_no) != OK) {
+    printf("mouse_test_packet: mouse_subscribe_int  failed\n");
+    return !OK;
+  }
+  uint32_t mouse_irq_set = BIT(mouse_bit_no);
+  uint32_t timer_irq_set = BIT(timer_bit_no);
+
+  uint8_t mouse_packet[3];
+  uint8_t byte_counter = 0;
+  uint8_t data;
+  bool is_sync = false;
+  struct packet pp;
+
+  extern unsigned counter;
+
+  while( counter < sys_hz() * idle_time ) {
+    if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) { 
+        printf("driver_receive failed with: %d", r);
+        continue;
+    }
+    if (is_ipc_notify(ipc_status)) { 
+      switch (_ENDPOINT_P(msg.m_source)) {
+          case HARDWARE: 
+              if (msg.m_notify.interrupts & timer_irq_set) {
+                timer_int_handler();
+              }
+              if (msg.m_notify.interrupts & mouse_irq_set) { 
+                mouse_ih();
+
+                if (check_mouse_error()) { continue; }
+                data = get_mouse_data();
+                
+                if (byte_counter == 0 && (data & MOUSE_FLAG)) { is_sync = true; }
+
+                if (is_sync) {
+                  mouse_packet[byte_counter] = data;
+
+                  if (byte_counter == 2) {
+                    is_sync = false;
+                    byte_counter = 0;
+                    pp = get_mouse_packet(mouse_packet);
+                    mouse_print_packet(&pp);
+                    counter = 0;
+                  } else { byte_counter++; }
+                }
+              }
+              break;
+          default:
+              break; 
+      }
+    }
+  }
+  if (timer_unsubscribe_int() != OK || mouse_unsubscribe_int() != OK) {
+    printf("mouse_test_packet: mouse_subscribe_int failed\n");
+    return !OK;
+  }
+  return set_mouse_data_reporting(false);
 }
 
 int (mouse_test_gesture)(uint8_t x_len, uint8_t tolerance) {
