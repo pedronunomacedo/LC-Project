650838e0189163665e52c4ab58807d37b0048d10
Fernando Rego <fernandoluisrego@gmail.com>
2022-04-30 12:37:28 +0100

    Add device subscribes and fix files indentation

--- proj/src/lib/devices/kbc.c
+++ proj/src/lib/devices/kbc.c
@@ -6,11 +6,11 @@ static uint8_t keyboard_data = 0x0;
 static bool keyboard_error = false;
 
 void (kbd_ih)(void) {
-  keyboard_error = false;
+	keyboard_error = false;
 	uint8_t st;
 	if (get_status_register(&st) != OK) {
-    printf("mouse_ih: util_sys_inb failed\n");
-  }
+		printf("mouse_ih: util_sys_inb failed\n");
+	}
 
 	if (st & KBC_OBF) {
 		if (util_sys_inb(KBC_OUT_BUF,&keyboard_data) != OK) {
@@ -48,70 +48,70 @@ uint8_t (get_keyboard_data)() { return keyboard_data; }
 bool (check_keyboard_error)() { return keyboard_error; }
 
 //mouse
-int mouse_hook_id = MOUSE_IRQ;
+int mouse_hook_id;
 static uint8_t mouse_data = 0x0;
 static bool mouse_error = false;
 
 void (mouse_ih)(void) {
-  uint8_t st;
-  mouse_error = false;
-  if (get_status_register(&st) != OK) {
-    printf("mouse_ih: util_sys_inb failed\n");
-  }
+	uint8_t st;
+	mouse_error = false;
+	if (get_status_register(&st) != OK) {
+		printf("mouse_ih: util_sys_inb failed\n");
+	}
 
-  if (st & KBC_OBF && st & KBC_AUX) {
-    if (util_sys_inb(KBC_OUT_BUF, &mouse_data) != OK) {
-      printf("mouse_ih: util_sys_inb failed\n");
-    }
+	if (st & KBC_OBF && st & KBC_AUX) {
+		if (util_sys_inb(KBC_OUT_BUF, &mouse_data) != OK) {
+			printf("mouse_ih: util_sys_inb failed\n");
+		}
 
-    if (st & KBC_PARITY_ERR || st & KBC_TIMEOUT_ERR) {
-      mouse_error = true;
-    }
-  }
+		if (st & KBC_PARITY_ERR || st & KBC_TIMEOUT_ERR) {
+			mouse_error = true;
+		}
+	}
 }
 
 int (mouse_subscribe_int)(uint8_t * bit_no) {
-  *bit_no = MOUSE_IRQ;
-  if (sys_irqsetpolicy(MOUSE_IRQ, IRQ_REENABLE | IRQ_EXCLUSIVE, &mouse_hook_id) != OK) {
-    printf("mouse_subscribe_int: sys_irqsetpolicy failed\n");
-    return !OK;
-  }
-  return OK;
+	*bit_no = mouse_hook_id = MOUSE_IRQ;
+	if (sys_irqsetpolicy(MOUSE_IRQ, IRQ_REENABLE | IRQ_EXCLUSIVE, &mouse_hook_id) != OK) {
+		printf("mouse_subscribe_int: sys_irqsetpolicy failed\n");
+		return !OK;
+	}
+	return OK;
 }
 
 int (mouse_unsubscribe_int)() {
-  if (sys_irqrmpolicy(&mouse_hook_id) != OK) {
-    printf("mouse_subscribe_int: sys_irqrmpolicy failed\n");
-    return !OK;
-  }
-  return OK;
+	if (sys_irqrmpolicy(&mouse_hook_id) != OK) {
+		printf("mouse_subscribe_int: sys_irqrmpolicy failed\n");
+		return !OK;
+	}
+	return OK;
 }
 
 int (set_mouse_data_reporting)(bool enable) {
-  uint8_t cmd = enable ? MOUSE_ENABLE_DATA_REPORTING : MOUSE_DISABLE_DATA_REPORTING;
-  while (1) {
-    wait_for_IB_is_clear();
-    if (sys_outb(KBC_CMD_REG, KBC_WR_BYTE_MOUSE) != OK) {
-      printf("set_mouse_data_reporting: sys_outb failed\n");
-      return !OK;
-    }
+	uint8_t cmd = enable ? MOUSE_ENABLE_DATA_REPORTING : MOUSE_DISABLE_DATA_REPORTING;
+	while (1) {
+		wait_for_IB_is_clear();
+		if (sys_outb(KBC_CMD_REG, KBC_WR_BYTE_MOUSE) != OK) {
+			printf("set_mouse_data_reporting: sys_outb failed\n");
+			return !OK;
+		}
 
-    wait_for_IB_is_clear();
-    if (sys_outb(KBC_ARG_REG, cmd) != OK) {
-      printf("set_mouse_data_reporting: sys_outb failed\n");
-      return !OK;
-    }
+		wait_for_IB_is_clear();
+		if (sys_outb(KBC_ARG_REG, cmd) != OK) {
+			printf("set_mouse_data_reporting: sys_outb failed\n");
+			return !OK;
+		}
 
-    uint8_t res = 0;
+		uint8_t res = 0;
 
-    if (util_sys_inb(MOUSE_READ_RES, &res) != OK) {
-      printf("set_mouse_data_reporting: sys_outb failed\n");
-      return !OK;
-    }
+		if (util_sys_inb(MOUSE_READ_RES, &res) != OK) {
+			printf("set_mouse_data_reporting: sys_outb failed\n");
+			return !OK;
+		}
 
-    if (res == MOUSE_ACK) { break; }
-  }
-  return OK;
+		if (res == MOUSE_ACK) { break; }
+	}
+	return OK;
 }
 
 uint8_t (get_mouse_data)() { return mouse_data; }
@@ -119,53 +119,53 @@ uint8_t (get_mouse_data)() { return mouse_data; }
 bool (check_mouse_error)() { return mouse_error; }
 
 struct packet (get_mouse_packet)(uint8_t * mouse_packet) {
-  struct packet pp;
+	struct packet pp;
 
-  for (int i = 0; i < 3; i++) { pp.bytes[i] = mouse_packet[i]; }
-  pp.lb = (mouse_packet[0] & MOUSE_LEFT_BUTTON);
-  pp.rb = (mouse_packet[0] & MOUSE_RIGHT_BUTTON);
-  pp.mb = (mouse_packet[0] & MOUSE_MIDDLE_BUTTON);
-  pp.delta_x = (mouse_packet[0] & MOUSE_MSB_X_DELTA) ? 0xff00 | mouse_packet[1] : mouse_packet[1];
-  pp.delta_y = (mouse_packet[0] & MOUSE_MSB_Y_DELTA) ? 0xff00 | mouse_packet[2] : mouse_packet[2];
-  pp.x_ov = (mouse_packet[0] & MOUSE_X_OVFL);
-  pp.y_ov = (mouse_packet[0] & MOUSE_Y_OVFL);
+	for (int i = 0; i < 3; i++) { pp.bytes[i] = mouse_packet[i]; }
+	pp.lb = (mouse_packet[0] & MOUSE_LEFT_BUTTON);
+	pp.rb = (mouse_packet[0] & MOUSE_RIGHT_BUTTON);
+	pp.mb = (mouse_packet[0] & MOUSE_MIDDLE_BUTTON);
+	pp.delta_x = (mouse_packet[0] & MOUSE_MSB_X_DELTA) ? 0xff00 | mouse_packet[1] : mouse_packet[1];
+	pp.delta_y = (mouse_packet[0] & MOUSE_MSB_Y_DELTA) ? 0xff00 | mouse_packet[2] : mouse_packet[2];
+	pp.x_ov = (mouse_packet[0] & MOUSE_X_OVFL);
+	pp.y_ov = (mouse_packet[0] & MOUSE_Y_OVFL);
 
-  return pp;
+	return pp;
 }
 
 bool (check_only_lb)(struct packet pp) { 
-  return pp.lb && !pp.rb && !pp.mb;
+	return pp.lb && !pp.rb && !pp.mb;
 }
 
 bool (check_none_pressed)(struct packet pp) { 
-  return !pp.lb && !pp.rb && !pp.mb;
+	return !pp.lb && !pp.rb && !pp.mb;
 }
 
 bool (check_only_rb)(struct packet pp) { 
-  return !pp.lb && pp.rb && !pp.mb;
+	return !pp.lb && pp.rb && !pp.mb;
 }
 
 // kbc
 int (get_status_register)(uint8_t * st) {
-  if (util_sys_inb(KBC_ST_REG, st) != OK) {
-    printf("get_status_register: util_sys_inb failed\n");
-    return !OK;
-  }
-  return OK;
+	if (util_sys_inb(KBC_ST_REG, st) != OK) {
+		printf("get_status_register: util_sys_inb failed\n");
+		return !OK;
+	}
+	return OK;
 }
 
 void (wait_for_IB_is_clear)() {
-  uint8_t st;
-  if (get_status_register(&st) != OK) {
-    printf("wait_for_IB_is_clear: get_status_register failed\n");
-    return;
-  }
-  while (st & KBC_IBF) {
-    tickdelay(micros_to_ticks(DELAY));
-    if (get_status_register(&st) != OK) {
-      printf("wait_for_IB_is_clear: get_status_register failed\n");
-      return;
-    }
-  }
-  return;
+	uint8_t st;
+	if (get_status_register(&st) != OK) {
+		printf("wait_for_IB_is_clear: get_status_register failed\n");
+		return;
+	}
+	while (st & KBC_IBF) {
+		tickdelay(micros_to_ticks(DELAY));
+		if (get_status_register(&st) != OK) {
+			printf("wait_for_IB_is_clear: get_status_register failed\n");
+			return;
+		}
+	}
+	return;
 }
