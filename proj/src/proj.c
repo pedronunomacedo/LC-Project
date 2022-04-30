8e95efb6cc44cc72b3380a6819253906e0e21b62
Fernando Rego <fernandoluisrego@gmail.com>
2022-04-30 12:12:20 +0100

    Add main loop

--- proj/src/proj.c
+++ proj/src/proj.c
@@ -5,11 +5,33 @@ int main(int argc, char *argv[]) {
 	lcf_set_language("EN-US");
 	lcf_trace_calls("/home/lcom/labs/proj/trace.txt");
 	lcf_log_output("/home/lcom/labs/proj/output.txt");
-	if (lcf_start(argc, argv) != OK) return !OK;
-	return lcf_cleanup();
+	if (lcf_start(argc, argv)) 
+		return !OK;
+	lcf_cleanup();
+	return OK;
 }
 
 int (proj_main_loop)(int argc, char *argv[]) {
-	printf("To implement\n");
-	return !OK;
+	int r, ipc_status;
+	message msg;
+	uint32_t irq_set = 0;
+
+	while( 1 ) {
+    	if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) { 
+			printf("driver_receive failed with: %d", r);
+			continue;
+    	}
+
+		if (is_ipc_notify(ipc_status)) { 
+			switch (_ENDPOINT_P(msg.m_source)) {
+				case HARDWARE: 
+					if (msg.m_notify.interrupts & irq_set) {
+						
+					}
+					break;
+				default:
+					break; 
+			}
+		}
+	}
 }
