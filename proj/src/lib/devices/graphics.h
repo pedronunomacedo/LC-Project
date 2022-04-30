34adac780fab64da636660498626840dfc8be1ec
Fernando Rego <fernandoluisrego@gmail.com>
2022-04-30 11:58:04 +0100

    Add initial project files

---
+++ proj/src/lib/devices/graphics.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <lcom/lcf.h>
+#include <machine/int86.h>
+#include <sys/mman.h>
+
+#include "vbe.h"
+
+int (set_video_mode)(uint16_t mode);
+
+void * (vg_init)(uint16_t mode);
+
+int (vg_swap_buffers)();
+
+int (vg_draw_matrix)(bool indexed_mode, uint8_t no_rectangles, uint32_t first, uint8_t step);
+
+int (vg_draw_rectangle)(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color);
+
+int (vg_draw_hline)(uint16_t x, uint16_t y, uint16_t len, uint32_t color);
+
+int (vg_draw_pixel)(uint16_t x, uint16_t y, uint32_t color);
+
+int (vg_draw_sprite)(uint8_t * sprite, xpm_image_t img, uint16_t x, uint16_t y);
+
+uint32_t (set_bits)(uint8_t start, uint8_t end);
+
+char* (vg_get_current_buffer)();
---
+++ proj/src/lib/devices/kbc.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <lcom/lcf.h>
+#include "i8042.h"
+
+//keyboard
+void (kbd_ih)(void);
+
+int (kbd_subscribe_int)(uint8_t *bit_no);
+
+int (kbd_unsubscribe_int)();
+
+uint8_t (get_keyboard_data)(void);
+
+bool (check_keyboard_error)(void);
+
+//mouse
+void (mouse_ih)(void);
+
+int (mouse_subscribe_int)(uint8_t * bit_no);
+
+int (mouse_unsubscribe_int)(void);
+
+int (set_mouse_data_reporting)(bool enable);
+
+void (wait_for_IB_is_clear)();
+
+uint8_t (get_mouse_data)(void);
+
+bool (check_mouse_error)(void);
+
+struct packet (get_mouse_packet)(uint8_t * mouse_packet);
+
+bool (check_only_rb)(struct packet pp);
+
+bool (check_only_lb)(struct packet pp);
+
+bool ((check_none_pressed))(struct packet pp);
+
+
+//kbc
+void (kbc_ih)(void);
+
+int (get_status_register)(uint8_t * st);
