945cc0adfc9bf35431a5b7392098199ac9bb5777
Fernando Rego <fernandoluisrego@gmail.com>
2022-04-30 23:59:23 +0100

    Add first events

--- proj/src/events/events.h
+++ proj/src/events/events.h
@@ -1,4 +1,8 @@
 #pragma once
+#include <lcom/lcf.h>
+
+#include "menu/main_menu.h"
+#include "lib/sprite/sprite.h"
 
 typedef enum {
     IN_GAME,
@@ -8,3 +12,9 @@ typedef enum {
     RULES,
     QUIT,
 } GAME_STATE;
+
+GAME_STATE (handle_timer_main_menu)();
+
+GAME_STATE (handle_keyboard_main_menu)(uint16_t scancode);
+
+GAME_STATE (handle_mouse_main_menu)(struct packet pp);
