1181daba5fcab8f835e80c05f5f73991587bdcb4
Fernando Rego <fernandoluisrego@gmail.com>
2022-05-01 21:20:55 +0100

    Add background image and optimize video device

--- proj/src/lib/devices/graphics.h
+++ proj/src/lib/devices/graphics.h
@@ -5,6 +5,7 @@
 #include <sys/mman.h>
 
 #include "vbe.h"
+#include "../sprite/sprite.h"
 
 int (vg_set_video_mode)(uint16_t mode);
 
@@ -18,8 +19,12 @@ int (vg_draw_hline)(uint16_t x, uint16_t y, uint16_t len, uint32_t color);
 
 int (vg_draw_pixel)(uint16_t x, uint16_t y, uint32_t color);
 
-int (vg_draw_sprite)(uint8_t * sprite, xpm_image_t img, uint16_t x, uint16_t y);
+int (vg_draw_sprite)(struct sprite * sprite);
+
+int (vg_draw_sprite_no_checks)(struct sprite * sp);
 
 uint32_t (set_bits)(uint8_t start, uint8_t end);
 
 char* (vg_get_current_buffer)();
+
+void (vg_clear_buffer)();
--- proj/src/menu/main_menu.c
+++ proj/src/menu/main_menu.c
@@ -1,19 +1,24 @@
 #include "main_menu.h"
 
 #include "../assets/mouse/mouse.xpm"
+#include "../assets/main_menu/background.xpm"
 
 static sprite * mouse_sprite;
+static sprite * background_sprite;
 
 int (initialize_main_menu)(uint32_t x, uint32_t y) {
     mouse_sprite = sprite_new(mouse_xpm, x, y);
-    if (mouse_sprite == NULL) {
+    background_sprite = sprite_new(background_xpm, 0, 0);
+    if (mouse_sprite == NULL || background_sprite == NULL) {
         return !OK;
     }
     return OK;
 }
 
 int (draw_main_menu)(void) {
-    return vg_draw_sprite(mouse_sprite->map, mouse_sprite->img, mouse_sprite->x, mouse_sprite->y);
+    if (vg_draw_sprite_no_checks(background_sprite) != OK) { return !OK; }
+    if (vg_draw_sprite(mouse_sprite) != OK) { return !OK; }
+    return OK;
 }
 
 sprite * get_mouse_sprite_main_menu() { return mouse_sprite; }
