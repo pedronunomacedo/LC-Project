77541655a43d6048e2be91ed2eb6de69b20a25c0
Fernando Rego <fernandoluisrego@gmail.com>
2022-05-12 16:26:34 +0100

    Comment main menu

--- proj/src/menu/main_menu.h
+++ proj/src/menu/main_menu.h
@@ -12,16 +12,52 @@
 #define RULES_BTN_Y 550
 #define QUIT_BTN_Y 700
 
+/**
+ * @brief Initialize main menu sprites
+ * 
+ * @param x 
+ * @param y 
+ * @return int Return 0 upon success and non-zero otherwise
+ */
 int (initialize_main_menu)(uint32_t x, uint32_t y);
 
+/**
+ * @brief Destroy main menu sprites
+ * 
+ */
 void (destroy_main_menu)(void);
 
+/**
+ * @brief Draw main menu in the screen
+ * 
+ * @return int Return 0 upon success and non-zero otherwise
+ */
 int (draw_main_menu)(void);
 
-sprite * get_mouse_sprite_main_menu();
+/**
+ * @brief Get the mouse sprite of the main menu
+ * 
+ * @return sprite* returns the mouse sprite of the main menu
+ */
+sprite * (get_mouse_sprite_main_menu)();
 
-sprite * get_play_sprite_main_menu();
+/**
+ * @brief Get the play sprite of the main menu
+ * 
+ * @return sprite* returns the play button sprite of the main menu
+ */
+sprite * (get_play_sprite_main_menu)();
 
-sprite * get_rules_sprite_main_menu();
+/**
+ * @brief Get the rules sprite of the main menu
+ * 
+ * @return sprite* returns the rules button sprite of the main menu
+ */
+sprite * (get_rules_sprite_main_menu)();
 
-sprite * get_quit_sprite_main_menu();
+/**
+ * @brief Get the quit sprite of the main menu
+ * 
+ * @return sprite* returns the quit button sprite of the main menu
+ */
+sprite * (get_quit_sprite_main_menu)();
