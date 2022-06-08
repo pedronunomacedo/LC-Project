33027e7586c3f334449076ac44860e8483449a69
Fernando Rego <fernandoluisrego@gmail.com>
2022-06-08 01:29:17 +0100

    Remove lab rewrites

--- lab4_rewrite/utils.c
+++
@@ -1,8 +0,0 @@
-#include <lcom/lcf.h>
-
-int (util_sys_inb)(int port, uint8_t *value) {
-    uint32_t input;
-    if (sys_inb(port, &input) != OK) { return !OK; }
-    *value = (uint8_t)input;
-    return OK;
-}
